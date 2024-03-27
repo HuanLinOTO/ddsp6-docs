86d560a1a4c4acdc24fe2fd47360a04978ba932f
HuanLinOTO <94176676+HuanLinOTO@users.noreply.github.com>
2024-03-27 09:27:34 +0000

    Deploying to gh-pages from @ gl-makers/ddsp5-docs@bf92bbd6f51ee1173862046c500d24e7637422e5 🚀

--- assets/app.p9W9TMFE.js
+++
@@ -1,7 +0,0 @@
-import{j as o,ao as i,ap as u,aq as l,ar as c,as as f,at as d,au as m,av as h,aw as A,ax as g,ay as v,d as P,u as y,l as w,z as C,az as _,aA as b,aB as E,ab as R}from"./chunks/framework.EAnijCdj.js";import{t as D}from"./chunks/theme.DDdo7hdG.js";function p(e){if(e.extends){const a=p(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const s=p(D),j=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=y();return w(()=>{C(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&_(),b(),E(),s.setup&&s.setup(),()=>R(s.Layout)}});async function x(){const e=O(),a=L();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function L(){return h(j)}function O(){let e=o,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&x().then(({app:e,router:a,data:t})=>{a.go().then(()=>{i(a.route,t.site),e.mount("#app")})});export{x as createApp};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
